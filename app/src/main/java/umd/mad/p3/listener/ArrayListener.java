aa7bef81e06e5e8fd47e1fd79476eb5d2d3b201e
Mitchell Myers <myersmk@umich.edu>
2020-04-02 00:12:16 -0400

    added custom listeners for volley

---
+++ app/src/main/java/umd/mad/p3/listener/ArrayListener.java
@@ -0,0 +1,14 @@
+package umd.mad.p3.listener;
+
+import com.android.volley.Response;
+
+import org.json.JSONArray;
+
+public class ArrayListener implements Response.Listener <JSONArray> {
+
+    @Override
+    public void onResponse(JSONArray response) {
+
+    }
+
+}
---
+++ app/src/main/java/umd/mad/p3/listener/ObjectListener.java
@@ -0,0 +1,12 @@
+package umd.mad.p3.listener;
+
+import com.android.volley.Response;
+
+import org.json.JSONObject;
+
+public class ObjectListener implements Response.Listener <JSONObject> {
+    @Override
+    public void onResponse(JSONObject response) {
+
+    }
+}
