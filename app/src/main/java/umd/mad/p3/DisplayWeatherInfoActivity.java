fc9760f390b7aa09ab00ef5af60d7bf707e89bda
Mitchell Myers <myersmk@umich.edu>
2020-04-03 20:05:14 -0400

    general weather layout defined and java associations made

--- app/src/main/java/umd/mad/p3/DisplayWeatherInfoActivity.java
+++ app/src/main/java/umd/mad/p3/DisplayWeatherInfoActivity.java
@@ -3,12 +3,31 @@ package umd.mad.p3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayWeatherInfoActivity extends AppCompatActivity {
+
+    OpenWeatherAPIReader weather;
+    TextView cityName;
+    TextView currentTemperatureValue;
+    TextView currentTemperatureUnitSymbol;
+    TextView ambientTemperatureValue;
+    TextView ambientTemperatureSymbol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_weather_info);
+
+        weather = new OpenWeatherAPIReader(this);
+
+        cityName = findViewById(R.id.cityName);
+        currentTemperatureValue = findViewById(R.id.currentTemperatureValue);
+        currentTemperatureUnitSymbol = findViewById(R.id.currentTemperatureUnit);
+        ambientTemperatureValue = findViewById(R.id.ambientTemperatureValue);
+        ambientTemperatureSymbol = findViewById(R.id.ambientTemperatureUnit);
+
+
+
     }
 }
